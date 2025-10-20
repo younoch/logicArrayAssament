18dc8cc329065cde0d4b038f0848941043161a6d
younoch <younoch295@outlook.com>
2025-10-20 23:24:27 +0600

    vue type  issue on vercel server

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+// This file is required for TypeScript to work with Vue single file components
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
@@ -9,4 +9,13 @@ declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     // Define any global properties here
   }
+}
+
+// Import and re-export Vue types to ensure they're available in all .vue files
+import 'vue'
+
+declare module 'vue' {
+  export * from '@vue/runtime-dom'
+  export * from '@vue/runtime-core'
+  export * from 'vue'
 }
